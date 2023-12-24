a29b4685478df3be8b13e7e04f7166a907f65db1
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-24 14:00:44 +0900

    Create 4_Profile.py

---
+++ pages/4_Profile.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config('EgGMAn', ':egg:', 'wide')
+st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_container_width=True)
+if st.sidebar.button('Clear Cache', use_container_width=True):
+    st.cache_data.clear()
+
+def profile(n, a, p, e):
+    st.subheader(n)
+    st.markdown(f'- Affiliation: {a}\n- Position: {p}\n- Mail: {e}')
+
+st.title('Profile')
+
+st.header('Developer')
+profile('Ryusei Hayashi', 'Nihon University, Grad. of Integrated Basic Sciences, Maj. of Earth Information Mathematical Sciences, Kitahara Lab', 'Student', '')
+
+st.header('Supporter')
+profile('Tetsuro Kitahara', 'Nihon University, Coll. of Humanities and Sciences, Dept. of Information Science, Kitahara Lab', 'Professor', '')
