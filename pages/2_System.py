25d8181f2b70fc8e645fe3027bb787280f1371d8
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2024-04-12 13:12:32 +0900

    Update 2_System.py

--- pages/2_System.py
+++ pages/2_System.py
@@ -14,18 +14,18 @@ view('1a167-_xiTrvA9JtiEfI8RST_T8996Stu', 700, 420)
 
 st.header('Text')
 
-st.subheader('Input Music')
-st.markdown('- Convert the input music to the coordinate z in VAE')
+st.subheader('Source Music')
+st.markdown('- Convert Source Music to the vector z in VAE')
 
 l, r = st.columns(2, gap='medium')
 
 with l:
-    st.subheader('Scene of Input Music')
-    st.markdown('- Collect music for the same scene as the input scene\n- Convert collected music to coordinates in VAE\n- Compute the center p of the coordinates')
+    st.subheader('Source Scene')
+    st.markdown('- Create a set of music to use in the same scene as Source Scene\n- Convert a set of music to a set of vector in VAE\n- Compute the center p of a set of vector')
 
 with r:
-    st.subheader('Scene of Output Music')
-    st.markdown('- Collect music for the same scene as the input scene\n- Convert collected music to coordinates in VAE\n- Compute the center q of the coordinates')
+    st.subheader('Target Scene')
+    st.markdown('- Create a set of music to use in the same scene as Target Scene\n- Convert a set of music to a set of vector in VAE\n- Compute the center q of a set of vector')
 
-st.subheader('Output Music')
-st.markdown('- Move the coordinate z in the vector q - p direction\n- Retrieve music near the moved coordinate z')
+st.subheader('Target Music')
+st.markdown('- Move the vector z in the vector q - p direction\n- Search music near the moved vector z')
