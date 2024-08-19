c78efe20fdb71d18ef9add9d5b53e73651bf088c
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2024-08-19 21:48:35 +0900

    Update 4_Profile.py

--- pages/4_Profile.py
+++ pages/4_Profile.py
@@ -3,14 +3,14 @@ import streamlit as st
 st.set_page_config('EgGMAn', ':musical_note:', 'wide')
 st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_container_width=True)
 
-def profile(n, a, p, e):
+def profile(n, a, p, m):
     st.subheader(n)
-    st.markdown(f'- Affiliation: {a}\n- Position: {p}\n- Mail: {e}')
+    st.markdown(f'- Affiliation: {a}\n- Position: {p}\n- Mail: {m}')
 
 st.title('Profile')
 
 st.header('Developer')
-profile('Ryusei Hayashi', 'Nihon University, Grad. of Integrated Basic Sciences, Maj. of Earth Information Mathematical Sciences, Kitahara Lab', 'Student', '')
+profile('Ryusei Hayashi', 'Nihon University, Grad. of Integrated Basic Sciences, Maj. of Earth Information Mathematical Sciences, Kitahara Lab.', 'Student', 'ryusei＠kthrlab.jp')
 
 st.header('Supporter')
-profile('Tetsuro Kitahara', 'Nihon University, Coll. of Humanities and Sciences, Dept. of Information Science, Kitahara Lab', 'Professor', '')
+profile('Tetsuro Kitahara', 'Nihon University, Coll. of Humanities and Sciences, Dept. of Information Science, Kitahara Lab.', 'Professor', 'kitahara＠kthrlab.jp')
