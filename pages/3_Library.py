881e9ed573733870f02be406133af98950842fca
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2024-08-19 21:47:40 +0900

    Update 3_Library.py

--- pages/3_Library.py
+++ pages/3_Library.py
@@ -3,17 +3,13 @@ import streamlit as st
 st.set_page_config('EgGMAn', ':musical_note:', 'wide')
 st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_container_width=True)
 
-def view(i, w, h):
-    s = f'https://drive.google.com/file/d/{i}/preview'
-    st.markdown(f'<iframe src="{s}" width="{w}" height="{h}"></iframe>', True)
+def iframe(u, w, h):
+    st.html(f'<iframe src="{u}/preview" width="{w}" height="{h}"></iframe>')
 
 st.title('Library')
 
 st.header('Paper')
-view('18H8dnbhL-D53BUWb09nGVWGWyot06OM6', 700, 990)
+iframe('https://drive.google.com/file/d/18H8dnbhL-D53BUWb09nGVWGWyot06OM6', 700, 990)
 
 st.header('Slide')
-view('1MIN0am-lZV7TqGx20CjgG1CSQXyLkgOO', 700, 420)
-
-st.header('Panel')
-view('1BY0bn5FWc_d2yoEi4ssPpgdomMCw9fLQ', 700, 990)
+iframe('https://drive.google.com/file/d/1MIN0am-lZV7TqGx20CjgG1CSQXyLkgOO', 700, 420)
