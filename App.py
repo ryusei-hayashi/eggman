9875027952d5415eae52d62d433d1bdb250ea61e
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2024-11-23 17:32:53 +0900

    Update App.py

--- App.py
+++ App.py
@@ -103,21 +103,6 @@ def music(u):
         return numpy.empty(0)
     st.markdown(f'<audio src="data:audio;base64,{b64encode(open(f"music.{e}", "rb").read()).decode()}" controlslist="nodownload" controls></audio>', True)
     return librosa.load(f'music.{e}', sr=sr, duration=30)[0]
-    try:
-        if 'soundcloud' in u:
-            SoundcloudAPI().resolve(u).write_mp3_to(open('music.mp3', 'wb+'))
-        elif 'youtube' in u:
-            open('music.mp3', 'wb').write(get(next(f['url'] for f in YouTube(u).streaming_data['adaptiveFormats'][::-1] if 'audio' in f['mimeType'])).content)
-        elif 'spotify' in u:
-            open('music.mp3', 'wb').write(get(sp.track(re.sub('intl-.*?/', '', u))['preview_url']).content)
-        else:
-            open('music.mp3', 'wb').write(get(u).content)
-    except:
-        if not u:
-            print(f'\033[31mError\033[0m: Unable to access {u}')
-        return numpy.empty(0)
-    st.markdown(f'<audio src="data:audio/mp3;base64,{b64encode(open("music.mp3", "rb").read()).decode()}" controlslist="nodownload" controls></audio>', True)
-    return librosa.load('music.mp3', sr=sr, duration=30)[0]
 
 def scene(c, s):
     with c:
