904f07ae4e4197ff788f7a470fc0da18bb7bcf7c
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2025-03-13 11:19:04 +0900

    Update 1_Usage.py

--- pages/1_Usage.py
+++ pages/1_Usage.py
@@ -6,22 +6,28 @@ st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_
 def iframe(u, w, h):
     st.markdown(f'<iframe src="{u}/preview" width="{w}" height="{h}"></iframe>', True)
 
+def scene(c, s, m):
+    with c:
+        st.header(s)
+        st.markdown(f'- Enter the scene that uses the {m}prepared music combining tags\n- There are about 200 tags such as opening, dungeon, etc')
+        st.subheader(f'Mood of {s}')
+        st.markdown('- Enter the mood of the scene with Valence-Arousal')
+        l, r = st.columns(2, gap='medium')
+        with l:
+            st.markdown('- Valence: Rises in positive scenes and falls in negative scenes')
+        with r:
+            st.markdown('- Arousal: Rises in active scenes and falls in passive scenes')
+
 st.title('Usage')
 
 iframe('https://drive.google.com/file/d/1X0O0hd9EHayuhPaUyPzXYJZljInnRtgO', 700, 420)
 
-st.subheader('Source Music')
-st.markdown('- Enter the URL of the music to use in developing game\n- URL support Spotify, SoundCloud, YouTube, Hotlink')
+st.header('Source Music')
+st.markdown('- Enter the music prepared for the developing game\n  - Web Service: Enter the URL of [these sites](https://github.com/yt-dlp/yt-dlp/blob/master/supportedsites.md)\n  - Direck Link: Enter the URL of the audio file\n  - Audio file: Enter the uploaded audio file')
 
-l, r = st.columns(2, gap='medium')
+c = st.columns(2, gap='medium')
+scene(c[0], 'Source Scene', 'prepared music')
+scene(c[1], 'Target Scene', 'searched music')
 
-with l:
-    st.subheader('Source Scene')
-    st.markdown('- Enter the scene where Source Music is used\n- There are about 200 scenes including "Opening", "Spring", etc')
-
-with r:
-    st.subheader('Target Scene')
-    st.markdown('- Enter the scene where Target Music is used\n- There are about 200 scenes including "Opening", "Spring", etc')
-
-st.subheader('Target Music')
-st.markdown('- Enter the site of No Copyright Music\n- EgGMAn search music for Target Scene in developing game\n- EgGMAn searche randomly if there is no Source Music')
+st.header('Target Music')
+st.markdown('- EgGMAn searches for game music of Target Scene in the developing game\n- EgGMAn randomly searches for game music of Target Scene without Source Music\n  - Ignore Artist: Enter the artist not to be listed\n  - Ignore Site: Enter the site not to be listed\n  - Time Range: Enter the time of music to be listed\n  - Random Scale: Enter the ratio to reflect music distribution')
