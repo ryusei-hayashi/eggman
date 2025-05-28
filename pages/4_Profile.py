45624c7d79aef45ebb91c85cd1cdce39be9b364e
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2025-05-28 14:54:47 +0900

    Update 4_Profile.py

--- pages/4_Profile.py
+++ pages/4_Profile.py
@@ -3,14 +3,14 @@ import streamlit as st
 st.set_page_config('EgGMAn', ':musical_note:', 'wide')
 st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_container_width=True)
 
-def profile(n, a, p, m):
+def profile(n, d):
     st.subheader(n)
-    st.markdown(f'- Affiliation: {a}\n- Position: {p}\n- Mail: {m}')
+    st.markdown('\n'.join(f'- {k}: {d[k]}' for k in d))
 
 st.title('Profile')
 
 st.header('Developer')
-profile('Ryusei Hayashi', 'Nihon University, Sound Computing Laboratory', 'Student', 'ryusei＠kthrlab.jp')
+profile('Ryusei Hayashi', {'Affiliation': 'Nihon University, Sound Computing Laboratory', 'Position': 'Student', 'Mail': 'ryusei＠kthrlab.jp'})
 
 st.header('Supporter')
-profile('Tetsuro Kitahara', 'Nihon University, Sound Computing Laboratory', 'Professor', 'kitahara＠kthrlab.jp')
+profile('Tetsuro Kitahara', {'Affiliation': 'Nihon University, Sound Computing Laboratory', 'Position': 'Professor', 'Mail': 'kitahara＠kthrlab.jp'})
